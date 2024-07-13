b1a758b54604ff59ab792495f6efe0354121aa76
Ömer Yazar <omeryazar0808@gmail.com>
2024-07-13 16:35:18 +0300

    feat: add authentication callbacks

--- auth.ts
+++ auth.ts
@@ -2,10 +2,27 @@ import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
 import authConfig from "./auth.config"
+import { db } from "./lib/db"
  
 const prisma = new PrismaClient()
  
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+        await db.user.update({
+            where: { id: user.id },
+            data: { emailVerified: new Date() }
+        })
+    }
+  },
+  callbacks: {
+      async session({ token, session }) {
+          if (token.sub && session.user) {
+              session.user.id = token.sub
+          }
+          return session
+      },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
