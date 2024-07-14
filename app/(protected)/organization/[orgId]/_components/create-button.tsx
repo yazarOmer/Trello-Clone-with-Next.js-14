85c329af4b1e5f52fbbd4e883ba03a73146508b6
Ömer Yazar <omeryazar0808@gmail.com>
2024-07-14 21:24:24 +0300

    fix: fixed some header button behaviours

--- app/(protected)/organization/[orgId]/_components/create-button.tsx
+++ app/(protected)/organization/[orgId]/_components/create-button.tsx
@@ -1,5 +1,10 @@
+import { FormPopover } from "@/components/form-popover";
 import { Button } from "@/components/ui/button";
 
 export const CreateButton = () => {
-  return <Button variant="secondary">Create</Button>;
+  return (
+    <FormPopover side="bottom" sideOffset={10} align="start">
+      <Button variant="secondary">Create</Button>
+    </FormPopover>
+  );
 };
--- app/(protected)/organization/[orgId]/_components/mobile-create-button.tsx
+++ app/(protected)/organization/[orgId]/_components/mobile-create-button.tsx
@@ -1,10 +1,13 @@
+import { FormPopover } from "@/components/form-popover";
 import { Button } from "@/components/ui/button";
 import { Plus } from "lucide-react";
 
 export const MobileCreateButton = () => {
   return (
-    <Button className="flex items-center justify-center">
-      <Plus size={16} />
-    </Button>
+    <FormPopover side="bottom" sideOffset={10} align="start">
+      <Button className="flex items-center justify-center">
+        <Plus size={16} />
+      </Button>
+    </FormPopover>
   );
 };
